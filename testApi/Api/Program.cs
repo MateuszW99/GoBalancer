465bc3dac7381f3a598e3a803ca6f268ee6088f9
Mateusz Wiczuk <mwiczuk99@gmail.com>
2025-07-21 23:05:53 +0200

    add logging and dummy endpoint

--- testApi/Api/Program.cs
+++ testApi/Api/Program.cs
@@ -1,10 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHttpLogging(_ => {});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseHttpLogging();
+
 if (app.Environment.IsDevelopment())
 {
 }
@@ -12,6 +14,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapGet("/healthcheck", () => new HealthCheckResponse("1.0.0", DateTime.UtcNow));
+
+app.MapGet("/hello-world", () => new { Message = "Hello, World!" });
 
 app.Run();
 
